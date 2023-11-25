77936b1f2901878c718bab96e302326c4b17ca2b
cocoxhuang <117036708+cocoxhuang@users.noreply.github.com>
2023-11-25 16:44:36 -0500

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from datetime import datetime
+import Util
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Header
+st.sidebar.header('Stock Network Discovery Dashboard `Demo`')
+
+# starting date
+st.sidebar.subheader('Starting date:')
+start = st.sidebar.text_input('Starting date (MM-DD-YYYY)', '01-01-2018')
+st.sidebar.write('The current starting date is', start)
+
+# ending date
+st.sidebar.subheader('Ending date:')
+end = st.sidebar.text_input('Ending date (MM-DD-YYYY)', '01-01-2023')
+st.sidebar.write('The current ending date is', end)
+
+# max number of stocks due to computational accurary of Glasso algo
+start_dt = datetime.strptime(start, '%m-%d-%Y').date()
+end_dt = datetime.strptime(end, '%m-%d-%Y').date()
+n_days = int( (end_dt-start_dt).days * 0.69)
+
+# number of stocks
+st.sidebar.subheader('Number of stocks:')
+n_stocks = st.sidebar.number_input('Select number of top weighted stocks in S&P 500:', min_value=0, max_value=n_days, value=20, step=1)
+n_stocks = int(n_stocks)
+st.sidebar.write(f'The number of stocks is {n_stocks}. At most {n_days} for computation accurary.')
+
+# Correlation method
+st.sidebar.subheader('Correlation method:')
+st.sidebar.write('Would you like to compute a network by thresholding correlations?')
+if_corr = st.sidebar.selectbox('Yes or no', ('Yes', 'No'))
+threshold = st.sidebar.number_input('If so, what is the threshold: ', 0.5)
+st.sidebar.write(f'There will be a connection between two stocks if their correlation is bigger than {threshold}')
+
+# Row A
+st.markdown('### Data')
+col1, col2, col3 = st.columns(3)
+col1.metric("Number of top weighted stocks", n_stocks, '')
+col2.metric("Start", start, '')
+col3.metric("End", end, '')
+
+X, embedding, names = Util.get_data(n_stocks,start_dt,end_dt)
+binary_adj = Util.glasso_adj(X)
+n_edges = binary_adj.sum()
+# Row B
+st.markdown('### [Glasso algorithm](%s) discovered latent stock network: %d connections' %('https://jerryfriedman.su.domains/ftp/glasso-bio.pdf',n_edges))
+G = Util.create_G(binary_adj)
+fig = Util.plot_network(G,embedding,names,'')
+# Row C
+st.plotly_chart(fig, use_container_width=True)
+
+if if_corr == 'Yes':
+    binary_adj = Util.cov_adj(X,threshold)
+    n_edges = binary_adj.sum()
+    # Row D
+    st.markdown(f'### Correlation method discovered latent stock network: {n_edges} connections')
+    G = Util.create_G(binary_adj)
+    fig = Util.plot_network(G,embedding,names,'')
+    # Row E
+    st.plotly_chart(fig, use_container_width=True)
---
+++ Util.py
@@ -0,0 +1,172 @@
+import numpy as np
+import networkx as nx
+import plotly.graph_objects as go
+from sklearn import covariance
+from sklearn.covariance import EmpiricalCovariance
+import pandas as pd
+import requests
+import yfinance as yf
+from sklearn.decomposition  import PCA
+
+def get_data(top,start,end):
+
+    # download the symbols of stocks
+    df = pd.read_html(requests.get('https://www.slickcharts.com/sp500',
+                        headers={'User-agent': 'Mozilla/5.0'}).text)[0]
+    symbols = list(df['Symbol'].values[:top])
+    names = list(df['Company'].values[:top])
+    print(f"The top {top} symbols in S&P 500 by weights order is: {symbols}")
+
+    # downloading data
+    data = yf.download(symbols, start=start, end=end)
+    open = data['Open']
+    close = data['Close']
+    variation = close - open
+
+    # remove NA stocks
+    symbols_remove = variation.isna().sum()[variation.isna().sum() != 0].keys()
+    names_remove = df[df['Symbol'].isin(symbols_remove)]['Company'].values
+    variation = variation.drop(columns=symbols_remove)
+    symbols = [x for x in symbols if x not in symbols_remove]
+    names = [x for x in names if x not in names_remove]
+
+    # standardize the time series: using correlations rather than covariance
+    # former is more efficient for structure recovery
+    X = variation.copy()
+    X /= X.std(axis=0)
+    
+    # get embedding for graphing using PCA
+    node_position_model = PCA(n_components=2)
+    embedding = node_position_model.fit_transform(X.T).T
+
+    return X, embedding, names
+
+def cov_adj(X,threshold=0.5):
+    cov = EmpiricalCovariance().fit(X)
+    emp_cov = cov.covariance_
+
+    det_emp_cov = np.linalg.det(emp_cov)
+    print(f"determinant of the MLE Empirical Covariance Matrix is : {det_emp_cov:2f}.")
+    if det_emp_cov < 0.0001:
+        print('The MLE Empirical Covariance Matrix is singular.')
+    else:
+        print('The MLE Empirical Covariance Matrix is NOT singular.')
+
+    d = 1 / np.sqrt(np.diag(emp_cov))
+    emp_corr = emp_cov*d*d[:, np.newaxis]
+    binary_adj_cov = np.abs(np.triu(emp_corr, k=1)) > threshold
+
+    return binary_adj_cov
+
+
+def glasso_adj(X):
+    alphas = np.logspace(-1.5, 1, num=10)
+    GLasso_model = covariance.GraphicalLassoCV(alphas=alphas)
+    GLasso_model.fit(X)
+
+    # Plot the graph of partial correlations
+    precision = GLasso_model.precision_.copy()
+    # Let the partial correlations matrix be partial_correlations
+    # Denote the (i,j)-th entry of partial_correlations be rho_ij
+    # Denote the (i,j)-th entry of precision matrix be p_ij
+    # rho_ij = - (p_ij) / sqrt(p_ii * p_ij)
+    d = 1 / np.sqrt(np.diag(precision))
+    partial_correlations = precision*d*d[:, np.newaxis]
+    binary_adj = np.abs(np.triu(partial_correlations, k=1)) > 0.02
+    binary_adj.astype(int)
+    return binary_adj
+
+
+def create_G(binary_adj):
+    np.fill_diagonal(binary_adj,False)
+
+    edge_index = torch.tensor(binary_adj).to_sparse().indices()
+    edges = list(zip(edge_index[0].tolist(),edge_index[1].tolist()))
+
+    import networkx as nx
+    G = nx.Graph()
+    pos = nx.get_node_attributes(G, "pos")
+    G.add_edges_from(edges)
+    G.add_nodes_from(np.arange(len(binary_adj)))
+
+    return G
+
+def plot_network(G,embedding,names,title):
+    edge_x = []
+    edge_y = []
+    pos = {i:embedding.T[i] for i in G.nodes}
+    nx.set_node_attributes(G, pos, "pos")
+
+    for edge in G.edges():
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            # colorscale options
+            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+            colorscale='YlGnBu',
+            reversescale=False,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    node_adjacencies = []
+    node_text = list(names)
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+        # node_text.append('# of connections: '+str(len(adjacencies[1])))
+        node_text[node] = node_text[node] + ', # of connections: '+str(len(adjacencies[1]))
+
+    node_trace.marker.color = node_adjacencies
+    node_trace.text = node_text
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                layout=go.Layout(
+                    title='<br>' + title,
+                    titlefont_size=16,
+                    showlegend=False,
+                    hovermode='closest',
+                    margin=dict(b=20,l=5,r=5,t=40),
+                    annotations=[ dict(
+                        # text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                        text='',
+                        showarrow=False,
+                        xref="paper", yref="paper",
+                        x=0.005, y=-0.002 ) ],
+                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    
+    return fig
